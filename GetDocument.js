025f539f8533c692161ed8d97f9b0bb2e76b9f69
terrence643 <terrence643@users.noreply.github.com>
2020-08-08 21:52:53 +0800

    Editing Aboutme
    Aboutme editing has been added to the js script

--- GetDocument.js
+++ GetDocument.js
@@ -1,3 +1,25 @@
+function renderAbout(doc){
+    let div = document.createElement('div');
+    let para = document.createElement('p');
+    let checkdiv = document.createElement('div');
+
+
+    para.textContent=doc.data().Intro;
+
+    div.appendChild(para);
+    div.appendChild(checkdiv);
+
+    document.getElementById('containerAbout').appendChild(div);
+}
+
+db.collection("Introduction").get().then(function(snapshot) {
+    snapshot.docs.forEach(function(doc){
+        renderAbout(doc);
+    })
+}).catch(function(Error){
+    console.log(Error)
+});
+
 function renderEduc(doc){
     
     let div = document.createElement('div');
@@ -7,31 +29,31 @@ function renderEduc(doc){
     let year_start=document.createElement('div');
     let year_end=document.createElement('div');
 
+ 
     div.setAttribute('data-id',doc.id);
     school.textContent = "School: "+doc.data().school;
     degree.textContent = "Degree: "+doc.data().degree;
     year_start.textContent = "Year Started: "+doc.data().year_start;
     year_end.textContent = "Year Ended: "+doc.data().year_end;
+  
 
-
-
-    div.appendChild(school);
+    div.appendChild(school);  
     div.appendChild(degree);
     div.appendChild(year_start);
     div.appendChild(year_end);
+    
 
     document.getElementById('Educ-list').appendChild(div);
+
 }
 
-    db.collection("education").get().then(function(snapshot) {
-        snapshot.docs.forEach(function(doc){
-            renderEduc(doc);
-        })
-    }).catch(function(Error){
-        console.log(Error)
+db.collection("education").get().then(function(snapshot) {
+    snapshot.docs.forEach(function(doc){
+        renderEduc(doc);
     })
-
-
+}).catch(function(Error){
+    console.log(Error)
+});
 
 function renderOrg(doc){
 
@@ -42,12 +64,12 @@ function renderOrg(doc){
     let org_year_start=document.createElement('div');
     let org_year_end=document.createElement('div');
 
+
     org_div.setAttribute('data-id',doc.id);
     org.textContent = "Name: "+doc.data().name;
     position.textContent = "Position: "+doc.data().position;
     org_year_start.textContent = "Year Started: "+doc.data().year_start;
     org_year_end.textContent = "Year Ended: "+doc.data().year_end;
-
 
 
     org_div.appendChild(org);
@@ -58,16 +80,19 @@ function renderOrg(doc){
     document.getElementById('Org-list').appendChild(org_div);
 }
 
-    db.collection("organizations").get().then(function(snapshot) {
-        snapshot.docs.forEach(function(doc){
-            renderOrg(doc);
-        })
-    }).catch(function(Error){
-        console.log(Error)
+db.collection("organizations").get().then(function(snapshot) {
+    snapshot.docs.forEach(function(doc){
+        renderOrg(doc);
     })
+}).catch(function(Error){
+    console.log(Error)
+})
+
+
 
 function renderProj(doc){
 
+    let proj_anch_div= document.createElement('div');
     let proj_anch = document.createElement('a');
     proj_anch.href=doc.data().Link;
     let proj_div = document.createElement('div');
@@ -79,62 +104,66 @@ function renderProj(doc){
 
     proj_div.setAttribute('data-id',doc.id);
 
-// INNER HTML METHOD
-//    proj_div.innerHTML += "URL/Project: <a href='"+doc.data().Link+"'>"+doc.data().Project+"</a>";
+    
     proj_div.textContent="Project: "+doc.data().Project;
     name.textContent = "Name: "+doc.data().name;
     proj_year_start.textContent = "Year Started: "+doc.data().year_start;
     proj_year_end.textContent = "Year Ended: "+doc.data().year_end;
-
+    proj_anch_div.textContent="Click Here to download the file";
 
 
     proj_div.appendChild(name);
     proj_div.appendChild(proj_year_start);
     proj_div.appendChild(proj_year_end);
+    proj_anch.appendChild(proj_anch_div);
+    proj_div.appendChild(proj_anch);
 
-    
-    proj_anch.appendChild(proj_div);
+    document.getElementById('Proj-list').appendChild(proj_div);
 
-    document.getElementById('Proj-list').appendChild(proj_anch);
 }
 
-    db.collection("works").get().then(function(snapshot) {
-        snapshot.docs.forEach(function(doc){
-            renderProj(doc);
-        })
-    }).catch(function(Error){
-        console.log(Error)
+db.collection("works").get().then(function(snapshot) {
+    snapshot.docs.forEach(function(doc){
+        renderProj(doc);
     })
+}).catch(function(Error){
+    console.log(Error)
+})
 
 
 function rendercontact(doc){
 
     let con_anch = document.createElement('a');
+    let con_anch_div=document.createElement('div');
     con_anch.href=doc.data().contact;
     let con_div = document.createElement('div');
     con_div.classList.add('Containercon');
 
-    let contact=document.createElement('div');
+    
     let name=document.createElement('div');
 
+
+    
     con_div.setAttribute('data-id',doc.id);
-   
+    con_div.textContent=doc.data().name;
+    con_anch_div.textContent="Click here Contact Me";
     
     //con_div.innerHTML += " <a href='"+doc.data().contact+"'>"+doc.data().name+"</a>";
     con_div.textContent = doc.data().name;
 
-
-
+    con_anch.appendChild(con_anch_div);
     con_div.appendChild(name);
-    con_anch.appendChild(con_div);
+    con_div.appendChild(con_anch);
 
-    document.getElementById('cont-list').appendChild(con_anch);
+    document.getElementById('cont-list').appendChild(con_div);
+
 }
 
-    db.collection("Contact").get().then(function(snapshot) {
-        snapshot.docs.forEach(function(doc){
-            rendercontact(doc);
-        })
-    }).catch(function(Error){
-        console.log(Error)
+db.collection("Contact").get().then(function(snapshot) {
+    snapshot.docs.forEach(function(doc){
+        rendercontact(doc);
     })
+}).catch(function(Error){
+    console.log(Error)
+})
+
